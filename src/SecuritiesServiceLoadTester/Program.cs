6b2c9c1f24ed063b71ac3d31b66b4f9d3721c00a
Adrian Cheong <adriancheong@gic.com.sg>
2016-10-13 11:53:46 +0800

    increasing call count to 5 for better scale differential

--- src/SecuritiesServiceLoadTester/Program.cs
+++ src/SecuritiesServiceLoadTester/Program.cs
@@ -77,7 +77,7 @@ namespace SecuritiesServiceLoadTester
             for (int i = 0; i < 10000; i++)
             {
                 stopwatch.Restart();
-                computeTest(1);
+                computeTest(5);
                 Console.WriteLine("Web server response time (seconds): {0} ", stopwatch.Elapsed.TotalSeconds);
             }
         }
